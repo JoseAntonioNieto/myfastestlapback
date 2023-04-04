8aff3013d12c01d55625ab8a3c1aafbd16477bff
Jose Antonio <gokugamer243@gmail.com>
2023-04-04 10:54:16 +0200

    feat: Conexion con base de datos

---
+++ src/database/database.js
@@ -0,0 +1,3 @@
+import Sequelize from "sequelize";
+
+export const sequelize = new Sequelize("postgres://gaia:alo33amr14*@192.168.168.130/reservas_circuitos")
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import user_data from "./user_data/user_data.js";
+import { sequelize } from "./database/database.js";
 
 const app =  express();
 
@@ -9,5 +10,12 @@ app.use(cors())
 app.use(express.json());
 
 app.use("/api", user_data);
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 app.listen(5000, () => console.log('Servidor iniciado en el puerto 5000'));
