515202a82ef42ca261e6f43d0a5dc305b7c5b42f
Jose Antonio <gokugamer243@gmail.com>
2023-04-14 17:39:53 +0200

    feat: Busqueda de los circuitos

--- src/circuitos/circuitos.js
+++ src/circuitos/circuitos.js
@@ -1,14 +1,62 @@
 import exppress from "express";
+import { Op } from "sequelize";
+import QueryTypes from "sequelize";
+import { sequelize } from "../database/database.js";
 import { Circuitos } from "../models/Circuitos.js";
 
 const circuitos = exppress.Router();
 
 circuitos.get("/circuitos", async (req, res) => {
+
+    const saltos = req.body.pagina * 9 - 9;
+
     try {
-        const circuitos = Circuitos.findAll();
+        let circuitos;
+
+        if (req.body.pais && req.body.nombre) {
+            const nombre = req.body.nombre;
+            let respuesta = await sequelize.query(`SELECT * FROM circuitos WHERE LOWER(nombre) LIKE '%${nombre.toLowerCase()}%' AND ubicacion = '${req.body.pais}' LIMIT 9 OFFSET ${saltos};`, {type: QueryTypes.SELECT})
+            circuitos = respuesta[0];
+            /*
+            await Circuitos.findAll({
+                where: {
+                    ubicacion: req.body.pais,
+                    nombre: {
+                        [Op.like]: `%${req.body.nombre}%`
+                    }
+                },
+                limit: 9,
+                offset: saltos
+            });
+            */
+        } else if (req.body.pais) {
+            circuitos = await Circuitos.findAll({
+                where: {
+                    ubicacion: req.body.pais
+                },
+                limit: 9,
+                offset: saltos
+            });
+        } else if (req.body.nombre) {
+            circuitos = await Circuitos.findAll({
+                where: {
+                    nombre: {
+                        [Op.like]: `%${req.body.nombre}%`
+                    }
+                },
+                limit: 9,
+                offset: saltos
+            });
+        } else {
+            circuitos = await Circuitos.findAll({
+                limit: 9,
+                offset: saltos
+            });
+        }
+        
         res.status(200).json(circuitos);
     } catch (err) {
-        res.status(401).send(err.message);
+        res.status(404).send(err.message);
     }
 });
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import express from "express";
 import cors from "cors";
 import user_data from "./user_data/user_data.js";
 import vehiculos from "./vehiculos/vehiculos.js";
+import circuitos from "./circuitos/circuitos.js";
 import { sequelize } from "./database/database.js";
 
 import "./models/Usuarios.js";
 import "./models/Vehiculos.js";
-import "./models/Usuarios_Vehiculos.js"
+import "./models/Usuarios_Vehiculos.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -27,5 +28,6 @@ try {
 
 app.use("/api", user_data);
 app.use("/api", vehiculos);
+app.use("/api", circuitos);
 
 app.listen(PORT, () => console.log('Servidor iniciado en el puerto 5000'));
