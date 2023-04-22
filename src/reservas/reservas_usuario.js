ba62ee8adf5249966f1311a5166de882e980108c
Jose Antonio <gokugamer243@gmail.com>
2023-04-22 19:54:04 +0200

    fix: Devuelve si la reserva se ha realizado

--- src/reservas/reservas_usuario.js
+++ src/reservas/reservas_usuario.js
@@ -48,20 +48,44 @@ reservas_usuario.post("/usuario/reservas", async (req, res) => {
     try {
         await verify(req.headers["authentication"]);
         const usuario_id = await getId(req.headers["authentication"]);
-        const reserva_usuario = await UsuariosReservas.create({
-            usuario_id: usuario_id,
-            id_reserva: req.body.id_reserva
-        });
 
-        const reserva_vehiculo = await VehiculosReservas.create({
-            id_reserva: req.body.id_reserva,
-            matricula: req.body.matricula
-        });
+        let respuesta;
 
-        res.status(200).json({
-            reserva_usuario: reserva_usuario,
-            reserva_vehiculo: reserva_vehiculo
-        });
+        const usuarioHaReservado = await UsuariosReservas.count({
+            where: {
+                usuario_id: usuario_id,
+                id_reserva: req.body.id_reserva
+            }
+        })
+
+        const vehiculoEstaEnReserva = await VehiculosReservas.count({
+            where: {
+                id_reserva: req.body.id_reserva,
+                matricula: req.body.matricula
+            }
+        })
+
+        if (usuarioHaReservado == 0 && vehiculoEstaEnReserva == 0) {
+            const reserva_usuario = await UsuariosReservas.create({
+                usuario_id: usuario_id,
+                id_reserva: req.body.id_reserva
+            });
+    
+            const reserva_vehiculo = await VehiculosReservas.create({
+                id_reserva: req.body.id_reserva,
+                matricula: req.body.matricula
+            });
+
+            respuesta = {
+                realizada: "si"
+            }
+        } else {
+            respuesta = {
+                realizada: "no"
+            }
+        }
+
+        res.status(200).json(respuesta);
     } catch (err) {
         res.status(401).send(err.message);
     }
