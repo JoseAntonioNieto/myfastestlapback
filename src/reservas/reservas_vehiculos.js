d0028cd41942cd8559a28a18b2afe46c29651f84
Jose Antonio <gokugamer243@gmail.com>
2023-04-30 10:52:39 +0200

    fix: Nombre  del circuito de la reserva

--- src/reservas/reservas_vehiculos.js
+++ src/reservas/reservas_vehiculos.js
@@ -2,7 +2,9 @@ import express from "express";
 import { verify, getId } from "../auth.js";
 import { Vehiculos } from "../models/Vehiculos.js";
 import { VehiculosReservas } from "../models/Vehiculos_Reservas.js";
+import { Reservas } from "../models/Reservas.js";
 import { Usuarios } from "../models/Usuarios.js";
+import { Circuitos } from "../models/Circuitos.js";
 
 const reservas_vehiculos = express.Router();
 
@@ -34,7 +36,22 @@ reservas_vehiculos.get('/vehiculos/reservas/:id', async (req, res) => {
                     }
                 });
 
-                arrVehiculos.push(vehiculo);
+                const reserva = await Reservas.findOne({
+                    where: {
+                        id_reserva: vehiculos[i].id_reserva,
+                    }
+                })
+
+                const circuito = await Circuitos.findOne({
+                    where: {
+                        id_circuito: reserva.id_circuito
+                    }
+                });
+
+                arrVehiculos.push({
+                    nombre_circuito: circuito.nombre,
+                    ...vehiculo.dataValues,
+                });
             }
 
             res.status(200).json(arrVehiculos);
