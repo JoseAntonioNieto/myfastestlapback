1aabfd56f6873d6611e14d6d9a1106ffa8dec341
Jose Antonio <gokugamer243@gmail.com>
2023-05-01 10:56:34 +0200

    fix: Vehiculo solo 1 usuario

--- src/vehiculos/vehiculos.js
+++ src/vehiculos/vehiculos.js
@@ -9,19 +9,34 @@ vehiculos.post("/vehiculos", async (req, res) => {
     try {
         await verify(req.headers["authentication"]);
         const usuario_id = await getId(req.headers["authentication"]);
-        const vehiculo = await Vehiculos.create({
-            matricula: req.body.matricula,
-            nombre_conductor: req.body.nombre_conductor,
-            dni_titular: req.body.dni_titular,
-            nombre_titular: req.body.nombre_titular
+
+        const vehiculo_existe = await Vehiculos.count({
+            where: {
+                matricula: req.body.matricula
+            }
         });
 
-        await UsuariosVehiculos.create({
-            matricula: req.body.matricula,
-            usuario_id: usuario_id
-        });
+        if (condition) {
+            const vehiculo = await Vehiculos.create({
+                matricula: req.body.matricula,
+                nombre_conductor: req.body.nombre_conductor,
+                dni_titular: req.body.dni_titular,
+                nombre_titular: req.body.nombre_titular
+            });
+    
+            await UsuariosVehiculos.create({
+                matricula: req.body.matricula,
+                usuario_id: usuario_id
+            });
+    
+            res.status(200).json(vehiculo);
+        } else {
+            res.status(200).json({
+                eliminado: false
+            });
+        }
 
-        res.status(200).json(vehiculo);
+        
     } catch (err) {
         res.status(401).send(err.message);
     }
